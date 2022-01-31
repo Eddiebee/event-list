9b6a20a4360550335b366865439f3199e69dfbe9
Eddiebee <edbeeotudor@gmail.com>
2022-01-31 18:08:58 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <p>
+          <code>All Your events! Unique Locations ! One Stop Reminder!</code>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;

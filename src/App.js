87189023bf770d2838e9ceab84bee09aa5908ccc
Eddiebee <edbeeotudor@gmail.com>
2022-02-01 09:17:50 +0100

    Initial App setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import Event from "./components/Event/Event";
+import "./App.css";
+
 function App() {
   return (
     <div className="App">
       <header>
-        <p>
-          <code>All Your events! Unique Locations ! One Stop Reminder!</code>
-        </p>
+        <h1>Events</h1>
       </header>
+      <Event />
     </div>
   );
 }

c5c76b947281fbb2fbce70ad8ae905f540d07c1d
Eddiebee <edbeeotudor@gmail.com>
2022-02-01 14:59:15 +0100

    Modify DateTime component to fit in

---
+++ src/components/Event/DateTimeDisplay.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const DateTimeDisplay = () => {
+  return (
+    <div className="dateTime">
+      <p className="date">Today</p>
+      <p className="time"> 17:00</p>
+    </div>
+  );
+};
+
+export default DateTimeDisplay;

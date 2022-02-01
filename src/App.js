e80394381f5bad3900a073d26f7b0f60f68055c5
Eddiebee <edbeeotudor@gmail.com>
2022-02-01 15:37:14 +0100

    Set up App component to supply data for event using the State Hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
+import { useState } from "react";
 import Event from "./components/Event/Event";
 import "./App.css";
 
 function App() {
+  const [events, setEvents] = useState([
+    {
+      title: "Bergen International Film Festival",
+      description:
+        "Films from all over the world gather all film enthusiasts for unique moments at the Bergen International Film Festival.",
+      date: "Today",
+      time: "17:00",
+    },
+    {
+      title: "Bergen International Film Festival",
+      description:
+        "Films from all over the world gather all film enthusiasts for unique moments at the Bergen International Film Festival.",
+      date: "Today",
+      time: "17:00",
+    },
+  ]);
   return (
     <div className="App">
       <header>
         <h1>Events</h1>
       </header>
-      <Event />
+      {/* Create an event component for each event in database */}
+      {events.map((event) => (
+        <Event key={event} event={event} />
+      ))}
     </div>
   );
 }

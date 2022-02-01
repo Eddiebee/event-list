b13f9f659848e399f5e2552389f1e183cac78a11
Eddiebee <edbeeotudor@gmail.com>
2022-02-01 15:03:13 +0100

    Build up AddToCalendar component to match-up with UI

--- src/components/Event/AddToCalendar.jsx
+++ src/components/Event/AddToCalendar.jsx
@@ -1,5 +1,12 @@
+import { AiOutlinePlus } from "react-icons/ai";
+
 const AddToCalendar = () => {
-  return <button>Add to Calendar</button>;
+  return (
+    <button className="addToCalendar">
+      <AiOutlinePlus />
+      &nbsp;Add to Calendar
+    </button>
+  );
 };
 
 export default AddToCalendar;

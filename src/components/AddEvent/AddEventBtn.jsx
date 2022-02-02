06b5a7689e8f81563f40005ad12d0391939d5c9e
Eddiebee <edbeeotudor@gmail.com>
2022-02-02 12:31:58 +0100

    Initial set up of AddEventBtn

---
+++ src/components/AddEvent/AddEventBtn.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { MdEvent } from "react-icons/md";
+
+const AddEventBtn = () => {
+  return (
+    <span role="button">
+      <MdEvent /> Add event
+    </span>
+  );
+};
+
+export default AddEventBtn;

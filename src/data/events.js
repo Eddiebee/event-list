0c9b785b04d4cc45c27b72048f79e9851a767f54
Eddiebee <edbeeotudor@gmail.com>
2022-02-02 12:02:10 +0100

    Set up data exclusively, will be using localStorage for deployment

---
+++ src/data/events.js
@@ -0,0 +1,30 @@
+export const data = [
+  {
+    title: "Bergen International Film Festival",
+    description:
+      "Films from all over the world gather all film enthusiasts for unique moments at the Bergen International Film Festival.",
+    date: "Today",
+    time: "17:00",
+  },
+  {
+    title: "Wool week",
+    description:
+      "ULLVEKA 2021 will be held for the eighth time in the period 22 - 31 October 2021, and will take place in the entire Bergen region.",
+    date: "22 - 31 Oct",
+    time: "10:00",
+  },
+  {
+    title: "Light park at Bergenhus Fortress",
+    description:
+      "LUMAGICA - a magical experience for young and old at Bergenhus Fortress, 12 November to 19 December 2021.",
+    date: "22 - 31 Oct",
+    time: "19:00",
+  },
+  {
+    title: "Gingerbread City 2021",
+    description:
+      "The world's largest Gingerbread Town can be found in the Xhibition shopping center, right in the center of Bergen",
+    date: "13 - 31 Dec",
+    time: "10:00",
+  },
+];

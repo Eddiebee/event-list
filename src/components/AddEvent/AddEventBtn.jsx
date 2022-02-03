cd06d80ac9dccd0524cda08f763f8fee37bd55f0
Eddiebee <edbeeotudor@gmail.com>
2022-02-03 13:41:33 +0100

    Add styles to the Event Component

--- src/components/AddEvent/AddEventBtn.jsx
+++ src/components/AddEvent/AddEventBtn.jsx
@@ -4,14 +4,13 @@ import "./AddEvent.css";
 
 const AddEventBtn = ({ showAddEvent, setShowAddEvent }) => {
   return (
-    <span
-      role="button"
+    <button
       tabIndex={0}
       onClick={() => setShowAddEvent(!showAddEvent)}
       className="AddEventBtn flip-in-hor-bottom"
     >
       <MdEvent /> Add Event
-    </span>
+    </button>
   );
 };
 

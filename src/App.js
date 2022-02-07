3f4e590c2d8aa52b6e2ea44973a6fb8df6cba131
Eddiebee <edbeeotudor@gmail.com>
2022-02-07 04:13:11 +0100

    Refactor App.js to share Data via DataProvider

--- src/App.js
+++ src/App.js
@@ -1,52 +1,18 @@
-import { useState } from "react";
-import { data } from "./data/events";
 import Event from "./components/Event/Event";
-import AddEvent from "./components/AddEvent/AddEvent";
 import AddEventBtn from "./components/AddEvent/AddEventBtn";
-import CloseAddEvent from "./components/AddEvent/CloseAddEvent";
+import { DataProvider } from "./context/DataContext";
 import "./App.css";
 
 function App() {
-  const [events, setEvents] = useState(data);
-  const [newEvent, setNewEvent] = useState({
-    title: "",
-    description: "",
-    date: "",
-    time: "",
-  });
-  const [showAddEvent, setShowAddEvent] = useState(false);
-
   return (
     <div className="App">
       <header>
         <h1>Events</h1>
       </header>
-      {/* Add Event btn to toggle Add Event form display */}
-      {showAddEvent ? (
-        <>
-          <span className="flip-in-hor-bottom addEventCnt">
-            <AddEvent
-              events={events}
-              setEvents={setEvents}
-              newEvent={newEvent}
-              setNewEvent={setNewEvent}
-            />
-            <CloseAddEvent
-              showAddEvent={showAddEvent}
-              setShowAddEvent={setShowAddEvent}
-            />
-          </span>
-        </>
-      ) : (
-        <AddEventBtn
-          showAddEvent={showAddEvent}
-          setShowAddEvent={setShowAddEvent}
-        />
-      )}
-      {/* Create an event component for each event in database */}
-      {events.map((event) => (
-        <Event key={event} event={event} />
-      ))}
+      <DataProvider>
+        <AddEventBtn />
+        <Event />
+      </DataProvider>
     </div>
   );
 }

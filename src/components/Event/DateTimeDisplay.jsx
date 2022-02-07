0dec09e57ea8cbfa113f1a2ddef4daead0754e11
Eddiebee <edbeeotudor@gmail.com>
2022-02-07 04:37:51 +0100

    Add a utility function to dynamically set the background color for the DateTimeDisplay component

--- src/components/Event/DateTimeDisplay.jsx
+++ src/components/Event/DateTimeDisplay.jsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const DateTimeDisplay = ({ date, time }) => {
+  const [selectedColor, setSelectedColor] = useState("eceff8");
+
+  useEffect(() => {
+    const selectedColors = ["#eceff8", "#eceff9", "#ecef77", "#ecef00"];
+    const chooseColor = (colors) => {
+      let randomIndex = Math.floor(Math.random() * colors.length);
+      return colors[randomIndex];
+    };
+
+    setSelectedColor(chooseColor(selectedColors));
+  }, []);
+
   return (
-    <div className="dateTime">
+    <div className="dateTime" style={{ backgroundColor: selectedColor }}>
       <p className="date">{date}</p>
       <p className="time"> {time}</p>
     </div>

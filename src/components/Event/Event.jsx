889c384183aa9b2b04368df1791457c3e63dad97
Eddiebee <edbeeotudor@gmail.com>
2022-02-07 04:14:48 +0100

    Refactor Event component to function with the useContext() hook

--- src/components/Event/Event.jsx
+++ src/components/Event/Event.jsx
@@ -1,21 +1,27 @@
+import { useContext } from "react";
 import AddToCalendar from "./AddToCalendar";
 import Description from "./Description";
 import Title from "./Title";
-import "./Event.css";
 import DateTimeDisplay from "./DateTimeDisplay";
+import DataContext from "../../context/DataContext";
+import "./Event.css";
 
-const Event = ({ event }) => {
-  const { date, time, title, description } = event;
-  return (
-    <div className="Event flip-in-hor-bottom">
-      <DateTimeDisplay date={date} time={time} />
-      <span className="details">
-        <Title title={title} />
-        <Description description={description} />
-        <AddToCalendar />
-      </span>
-    </div>
-  );
+const Event = () => {
+  const { events } = useContext(DataContext);
+
+  return events.map((event) => {
+    const { date, time, title, description } = event;
+    return (
+      <div className="Event flip-in-hor-bottom">
+        <DateTimeDisplay date={date} time={time} />
+        <span className="details">
+          <Title title={title} />
+          <Description description={description} />
+          <AddToCalendar />
+        </span>
+      </div>
+    );
+  });
 };
 
 export default Event;
